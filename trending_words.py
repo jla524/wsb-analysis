10c39439c4ab839894ea932985e9b3fcd7179dbe
Jacky Lee <jla524@sfu.ca>
2021-05-12 21:38:26 -0700

    Change keywords to be a global variable

--- trending_words.py
+++ trending_words.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 from helper import get_word_count
 
 
+# Define keywords to plot
+keywords = ['buy', 'GME', 'AMC', 'NOK', 'to the moon']
+
+
 if __name__ == '__main__':
     data = pd.read_csv('reddit_wsb.csv')
     data['date'] = pd.to_datetime(data['timestamp']).dt.round('D')
     fig = plt.figure(figsize=(16, 9)) 
     plt.title('Keyword count over time')
 
-    for word in ['buy', 'GME', 'AMC', 'NOK', 'to the moon']:
-        count_df = get_word_count(data, word)
-        plt.plot(count_df['date'], count_df['count'], label=word)
+    for keyword in keywords:
+        count_df = get_word_count(data, keyword)
+        plt.plot(count_df['date'], count_df['count'], label=keyword)
    
     plt.legend()
     plt.show()
