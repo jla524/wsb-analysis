64bbbb2fc0dde6c343dcb7c8fd7e28f2dbd7a579
Jacky Lee <jla524@sfu.ca>
2021-07-30 17:43:33 -0700

    Refactor word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,11 +18,11 @@ if __name__ == '__main__':
     title = data['title'].apply(clean_text)
     body = data['body'].fillna('').apply(clean_text)
     words = np.concatenate((title, body), axis=None)
-    
+
     # Create and display word cloud
-    wc = WordCloud(width=1600, height=900, background_color='white') \
-            .generate(''.join(words))
-    plt.figure(figsize=(16,9))
+    wc = WordCloud(width=1600, height=900, background_color='white')
+    wc.generate(''.join(words))
+    plt.figure(figsize=(16, 9))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
     plt.show()
