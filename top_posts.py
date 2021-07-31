184d59a700cf7d4547a78f6945b6537cbfc1b44f
Jacky Lee <jla524@sfu.ca>
2021-07-30 17:54:09 -0700

    Refactor top_posts.py

--- top_posts.py
+++ top_posts.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python3
 import pandas as pd
-
 
 # Define number of rows (n) and columns to print
 n = 50
 columns = ['title', 'score', 'url', 'timestamp']
-
 
 if __name__ == '__main__':
     data = pd.read_csv('reddit_wsb.csv')
