02d05aeed0695d7ada36d985c26db07ffe6610d3
Jacky Lee <jla524@sfu.ca>
2021-08-03 20:02:48 -0700

    Rename variables in helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 import pandas as pd
 
 
 def get_word_count(df: pd.DataFrame, word: str) -> pd.DataFrame:
     """Compute the number of times word is mentioned on WSB"""
     df.dropna(subset=['body'], inplace=True)
-    word_in_title = df['title'].str.count(word)
-    word_in_body = df['body'].str.count(word)
-    word_found = df[(word_in_title > 0) | (word_in_body > 0)]
+    words_in_title = df['title'].str.count(word)
+    words_in_body = df['body'].str.count(word)
+    word_found = df[(words_in_title > 0) | (words_in_body > 0)]
     word_count = word_found.groupby('date').count().reset_index()
     return word_count.rename(columns={'title': 'count'})
